869e926f762837ab3aab43b91c19eff187aec068
njoking <njokingugi9462@gmail.com>
2023-10-31 17:49:33 -0700

    task 0

---
+++ 1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+last digit = abs(number)%10
+print("The last digit of")
+print("number")
+print("the string is", last_digit, end=" ")
+if last digit > 5 :
+    print("and is greatter than 5")
+else if last digit ==0
+    print("and is 0")
+else if last digit < 6 && =! 0
+    print("and is less than 6 an not 0")
