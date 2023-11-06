5ba0f5869150ac98d4fbace64bbf990ef8924d00
njoking <njokingugi9462@gmail.com>
2023-11-06 09:45:23 -0800

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,5 +4,5 @@ def replace_in_list(my_list, idx, element):
     if (idx < 0 or idx > listlength):
         return (my_list)
     else:
-        listlength = element
+        my_list[idx] = element
         return (my_list)
