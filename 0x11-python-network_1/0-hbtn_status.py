7b71acf48a72a778cfc717910c70616be4d23a1c
njoking <njokingugi9462@gmail.com>
2024-03-06 02:58:21 +0300

    now

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status."""
-from urllib.request import Request, urlopen
+"Module that fetches https://alx-intranet.hbtn.io/status"
+import urllib.request
 
 
 if __name__ == "__main__":
-    req = Request("https://alx-intranet.hbtn.io/status")
-    with urlopen(req) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
