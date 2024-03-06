f583b837ea2bcdcf6cfbb743fa08c940189ee9ce
njoking <njokingugi9462@gmail.com>
2024-03-06 03:00:33 +0300

    now

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"Module that fetches https://alx-intranet.hbtn.io/status"
-import urllib.request
-
+"""0x11. Python - Network #1, task 0. What's my status? #0
+"""
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+    from urllib import request
+
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+    # charset can be gained with response.headers.get_content_charset()
