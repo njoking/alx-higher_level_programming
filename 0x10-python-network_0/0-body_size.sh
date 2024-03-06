9a234c984de3e1e5a5531fa5db98d6f3a74de9d1
njoking <njokingugi9462@gmail.com>
2024-03-06 03:08:54 +0300

    now

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+# displays body size of HTTP response in bytes
+curl -sI "$1" | grep 'Content-Length' | sed 's/^Content-Length: //'
