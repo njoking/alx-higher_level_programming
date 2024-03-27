6f43fee9d03296339ccb5a6cc4973fe1e4411473
njoking <njokingugi9462@gmail.com>
2024-03-27 23:33:01 +0300

    higher level

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-/**
- * This is a script that reads and prints the content of a file.
- * The first argument is the file path
- */
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
 });
