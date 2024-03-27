b4ab2126b6ed08b3af2b325e1b291bff9f06f2a1
njoking <njokingugi9462@gmail.com>
2024-03-27 23:27:20 +0300

    higher level

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-const process = require('process');
+/**
+ * This is a script that reads and prints the content of a file.
+ * The first argument is the file path
+ */
 const fs = require('fs');
-
-// The first argument is the file path
-const file = process.argv[2];
-// The content of the file must be written in utf-8
-fs.readFile(file, 'utf8', function (err, data) {
-  // If an error occurred during the reading, print the error object
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
